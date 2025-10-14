167763919f1b665996a8b602803cb83106c79b42
haiquanng <nguyenhaiquan.data@gmail.com>
2025-10-14 17:13:18 +0700

    feat: update sonnor with color, close button

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -4,12 +4,15 @@ import { Toaster as Sonner } from 'sonner';
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = 'system' } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <Sonner
-      theme={theme as ToasterProps['theme']}
+      theme={(theme ?? 'light') as ToasterProps['theme']}
       className='toaster group'
+      richColors={props.richColors ?? true}
+      closeButton={true}
+      duration={props.duration ?? 3000}
       toastOptions={{
         classNames: {
           toast:
