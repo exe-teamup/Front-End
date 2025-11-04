485cd2a678e106ef3735a645c475d32ccff56b55
ThanhChuse1234 <thanhcqse184888@fpt.edu.vn>
2025-11-04 10:35:44 +0700

    fix: remove old Moderator page exports

--- src/pages/Moderator/index.ts
+++ src/pages/Moderator/index.ts
@@ -1,11 +1,4 @@
-// Old Tailwind pages (backup)
-export { default as ModeratorHomepage } from './ModeratorHomepage';
-export { default as GroupOverview } from './GroupOverview';
-export { default as LecturerWorkload } from './LecturerWorkload';
-export { default as AcademicSettings } from './AcademicSettings';
-export { default as AuditLog } from './AuditLog';
-
-// New shadcn/ui pages with named exports
+// shadcn/ui pages with named exports
 export { Dashboard } from './Dashboard';
 export { WorkloadGiangVien } from './WorkloadGiangVien';
 export { GroupOverviewManagement } from './GroupOverviewManagement';
